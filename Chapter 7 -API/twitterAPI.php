693a23f5a2ea74eaa2ad4529f69d77208612f1a0
Deon Steyn <32790719+Steyn81@users.noreply.github.com>
2022-06-17 21:59:44 +0200

    Mobile and Python section

--- Chapter 7 -API/twitterAPI.php
+++ Chapter 7 -API/twitterAPI.php
@@ -1,37 +1,37 @@
 <?php
 
-require "twitteroauth/autoload.php";
+    require "twitteroauth/autoload.php";
 
-use Abraham\TwitterOAuth\TwitterOAuth;
+    use Abraham\TwitterOAuth\TwitterOAuth;
 
-$consumerkey = "dZ52MCrsr9T7ZZ0e3xJrBaQVS";
+    $consumerkey = "dZ52MCrsr9T7ZZ0e3xJrBaQVS";
 
-$consumersecret = "nyZy9jEzSv0IA9M4QaGrlaUBHHHP0DuqA1uIkFPR4ATpDid5iE";
+    $consumersecret = "nyZy9jEzSv0IA9M4QaGrlaUBHHHP0DuqA1uIkFPR4ATpDid5iE";
 
-$accesstoken = "703174801402138626-G6mNl13RXKKKBl2KwXl8icXUD4JqAlA";
+    $accesstoken = "703174801402138626-G6mNl13RXKKKBl2KwXl8icXUD4JqAlA";
 
-$accesstokensecret = "j5x1CwDReoTYRxummy2YivGMgP37q2pefPt9ry2tMMomO";
+    $accesstokensecret = "j5x1CwDReoTYRxummy2YivGMgP37q2pefPt9ry2tMMomO";
 
-$connection = new TwitterOAuth($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
-$content = $connection->get("account/verify_credentials");
+    $connection = new TwitterOAuth($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
+    $content = $connection->get("account/verify_credentials");
 
 
-$statuses = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_replies" => true]);
+    $statuses = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_replies" => true]);
 
-foreach ($statuses as $tweet) {
-    
-    
-    if ($tweet->favorite_count >= 2) {
+    foreach ($statuses as $tweet) {
         
-        $status = $connection->get("statuses/oembed", ["id" => $tweet->id]);
         
-        echo $status->html;
+        if ($tweet->favorite_count >= 2) {
+            
+            $status = $connection->get("statuses/oembed", ["id" => $tweet->id]);
+            
+            echo $status->html;
+            
+        }
+            
+        
+        
         
     }
-        
-    
-    
-    
-}
 
 ?>
